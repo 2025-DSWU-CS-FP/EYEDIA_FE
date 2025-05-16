54d7b0fdf6a36910430a82d525e2fd8df2bd6372
Dubabbi <thdms3160@naver.com>
2025-05-16 10:01:35 +0900

    feat: 전시 카드 컴포넌트에 skeleton 적용

--- src/components/main/ExhibitionCard.tsx
+++ src/components/main/ExhibitionCard.tsx
@@ -1,3 +1,8 @@
+import { useState, useEffect } from 'react';
+
+import Skeleton from 'react-loading-skeleton';
+
+import 'react-loading-skeleton/dist/skeleton.css';
 import { ExhibitionCardProps } from '@/types';
 
 export default function ExhibitionCard({
@@ -5,18 +10,31 @@ export default function ExhibitionCard({
   title,
   location,
 }: ExhibitionCardProps) {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = imageUrl;
+    img.onload = () => setLoaded(true);
+  }, [imageUrl]);
+
   return (
     <div className="w-40 flex-shrink-0">
-      <img
-        src={imageUrl}
-        alt={title}
-        className="h-40 w-full rounded-sm object-cover"
-      />
-      <h3 className="w-32 justify-start text-black text-base font-medium leading-tight pt-2 pb-1">
-        {title}
+      {!loaded ? (
+        <Skeleton height={160} borderRadius={4} />
+      ) : (
+        <img
+          src={imageUrl}
+          alt={title}
+          className="h-40 w-full rounded-sm object-cover"
+        />
+      )}
+
+      <h3 className="w-32 text-black text-base font-medium leading-tight pt-2 pb-1">
+        {loaded ? title : <Skeleton width={100} />}
       </h3>
-      <p className="w-32 justify-start text-neutral-400 text-xs font-medium">
-        {location}
+      <p className="w-32 text-neutral-400 text-xs font-medium">
+        {loaded ? location : <Skeleton width={80} />}
       </p>
     </div>
   );
