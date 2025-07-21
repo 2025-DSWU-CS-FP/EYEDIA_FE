687fd326c92051cbba1528b73b5ee22a94526839
Dubabbi <thdms3160@naver.com>
2025-07-22 03:23:48 +0900

    style: 기본 스타일 수정

--- src/layouts/NavigationBar.tsx
+++ src/layouts/NavigationBar.tsx
@@ -48,7 +48,7 @@ export default function NavigationBar() {
   ];
 
   return (
-    <div className="fixed bottom-0 left-1/2 -translate-x-1/2 z-10 flex w-full max-w-md items-center justify-around rounded-t-sm bg-[#F6F6F6] h-[70px] px-2">
+    <div className="fixed bottom-0 left-1/2 -translate-x-1/2 z-10 flex w-full max-w-[430px] items-center justify-around rounded-t-sm bg-[#F6F6F6] h-[70px] px-2">
       {menuList.map(menu => (
         <Link
           key={menu.to}
