6501d61fe6f8c33a2af6ac0c5b5239a8d775ca83
Dubabbi <thdms3160@naver.com>
2025-07-29 23:06:32 +0900

    feat: 쿼리 팩토리 패턴 적용

--- src/services/mutations/useLogin.ts
+++ src/services/mutations/useLogin.ts
@@ -1,18 +1,11 @@
 import { useMutation } from '@tanstack/react-query';
 
-import axiosInstance from '@/services/axiosInstance';
+import mutationFactory from '@/services/mutationFactory';
 import type { LoginRequest, LoginResponse } from '@/types/auth';
 
 const useLogin = () => {
-  return useMutation({
-    mutationFn: async (data: LoginRequest): Promise<LoginResponse> => {
-      const res = await axiosInstance.post('/api/v1/auth/login', data);
-
-      const { token } = res.data;
-      localStorage.setItem('accessToken', token);
-
-      return res.data;
-    },
+  return useMutation<LoginResponse, Error, LoginRequest>({
+    mutationFn: data => mutationFactory.login(data),
   });
 };
 
