a9df7ab233b3f01bc26db2810e7b5616f0c8ed2d
Dubabbi <thdms3160@naver.com>
2025-08-28 11:27:36 +0900

    style: 인기 전시 상세 ui 완성

--- src/pages/main/popular/PopularDetail.tsx
+++ src/pages/main/popular/PopularDetail.tsx
@@ -1,17 +1,11 @@
-import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-import ExhibitionCard from '@/components/gallery/ExhibitionCard';
+import Ticket from '@/assets/images/ticket.png';
 import PopularInfoCard from '@/components/main/PopularInfoCard';
 import Header from '@/layouts/Header';
-import { artworks, exhibitionInfo } from '@/mock/galleryDetailData';
+import { exhibitionInfo } from '@/mock/galleryDetailData';
 
 export default function PopularDetailPage() {
-  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
-
-  const handleCardClick = (index: number) => {
-    setSelectedIndex(index);
-  };
-
   return (
     <div className="pb-[4rem]">
       <Header
@@ -21,30 +15,30 @@ export default function PopularDetailPage() {
       />
 
       <div className="flex flex-col gap-[4rem]">
-        {selectedIndex === null && (
-          <>
-            <div className="px-[2.4rem]">
-              <PopularInfoCard
-                thumbnail={exhibitionInfo.thumbnail}
-                title={exhibitionInfo.title}
-                location={exhibitionInfo.location}
-              />
+        <div className="px-[2.4rem]">
+          <PopularInfoCard
+            thumbnail={exhibitionInfo.thumbnail}
+            title={exhibitionInfo.title}
+            location={exhibitionInfo.location}
+          />
+        </div>
+        <img
+          alt="인기 전시 상세 이미지"
+          src={exhibitionInfo.thumbnail}
+          className="px-[2.4rem]"
+        />
+        <Link
+          to="/card"
+          className="gradient-rotate gradient-dramatic shine relative mx-[2rem] flex h-[9.6rem] items-center justify-between gap-[1.3rem] rounded-[10px] bg-gradient-to-r from-cyan-200 via-blue-300 to-indigo-300 px-[2.4rem]"
+        >
+          <div className="flex flex-col justify-start gap-[0.4rem]">
+            <div className="whitespace-nowrap text-left text-gray-800 bt3">
+              친구와 함께 전시를 관람하고 싶다면?
             </div>
-            <div className="grid grid-cols-2 gap-[1.2rem] px-[2.4rem]">
-              {artworks.map((art, idx) => (
-                <ExhibitionCard
-                  key={art.id}
-                  imageUrl={art.imageUrl}
-                  title={art.title}
-                  subTitle={art.subTitle}
-                  showArrow={art.showArrow}
-                  onClick={() => handleCardClick(idx)}
-                  isSelected={false}
-                />
-              ))}
-            </div>
-          </>
-        )}
+            <div className="text-left text-black t3">Eyedia 공유하기 →</div>
+          </div>
+          <img className="max-w-[8rem]" src={Ticket} alt="티켓이미지" />
+        </Link>
       </div>
     </div>
   );
